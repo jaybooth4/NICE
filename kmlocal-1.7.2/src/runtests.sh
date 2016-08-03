462d01c167b92a4a4cb2756efe067ddc57c26d8f
Jason Booth <booth.j@husky.neu.edu>
2016-08-03 08:17:23 -0400

    Tried to run large data set, too large

--- kmlocal-1.7.2/src/runtests.sh
+++ kmlocal-1.7.2/src/runtests.sh
@@ -1,7 +1,8 @@
-time ./kmlsample -d 4 -k 2 -max 200000 -df ~/NICE/PresData2/kvar/data_k2_p100000_d4_c1.txt
-time ./kmlsample -d 4 -k 4 -max 200000 -df ~/NICE/PresData2/kvar/data_k4_p50000_d4_c1.txt
-time ./kmlsample -d 4 -k 6 -max 200000 -df ~/NICE/PresData2/kvar/data_k6_p33333_d4_c1.txt 
-time ./kmlsample -d 4 -k 8 -max 200000 -df ~/NICE/PresData2/kvar/data_k8_p25000_d4_c1.txt 
-time ./kmlsample -d 4 -k 10 -max 200000 -df ~/NICE/PresData2/kvar/data_k10_p20000_d4_c1.txt
+time ./kmlsample -d 100 -k 1000 -max 1000000 -df data_k1000_p1000_d100_c100.txt
+#time ./kmlsample -d 1000 -k 10 -max 1000000 -df data_k10_p100000_d1000_c1.txt
+#time ./kmlsample -d 100 -k 10 -max 1000000 -df data_k4_p100000_d10_c1.txt
+#time ./kmlsample -d 4 -k 6 -max 200000 -df ~/NICE/PresData2/kvar/data_k6_p33333_d4_c1.txt 
+#time ./kmlsample -d 4 -k 8 -max 200000 -df ~/NICE/PresData2/kvar/data_k8_p25000_d4_c1.txt 
+#time ./kmlsample -d 4 -k 10 -max 200000 -df ~/NICE/PresData2/kvar/data_k10_p20000_d4_c1.txt
 
 
